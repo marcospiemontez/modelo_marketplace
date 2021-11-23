efe4698a59a426577daca00cd339cd0b27b13dc5
Marcos Piemontez <marcos.piemontez1@hotmail.com>
2021-11-23 10:20:30 -0300

    📝 DOC: Adicionado configuração de rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -10,6 +10,33 @@ const routes = [
       }
     ]
   },
+  // ------------------------ LAYOUT DASHBOARD ------------------------
+  {
+    path: '/',
+    component: () => import('layouts/LayoutDash.vue'),
+    children: [
+      { path: '/dashboardAdmin',
+        name: 'dashboardAdmin',
+        component: () => import('src/pages/access/Admin/dashboardAdmin')
+      },
+      { path: 'perfil',
+        name: 'perfil',
+        component: () => import('src/pages/access/Admin/profile')
+      },
+      { path: '/produtos',
+        name: 'produtos',
+        component: () => import('src/pages/access/Admin/products')
+      },
+      { path: '/pedidos',
+        name: 'pedidos',
+        component: () => import('src/pages/access/Admin/requests')
+      },
+      { path: '/configuracao',
+        name: 'configuracao',
+        component: () => import('src/pages/access/Admin/settings')
+      }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
